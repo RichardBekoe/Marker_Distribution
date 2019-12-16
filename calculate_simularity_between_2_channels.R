849066db42f1a0cd6710fc89c6ff7df5c0323ad4
RichardBekoe <richardbekoe@gmail.com>
2019-12-16 14:00:56 +0000

    Positive interactions code created and plotted

--- calculate_simularity_between_2_channels.R
+++ calculate_simularity_between_2_channels.R
@@ -39,31 +39,39 @@ calculate_score2 <- function(o1, o2){
 calculate_score3 <- function(o1, o2){
   # product of Zernike moments projected onto the connecting line
 
-  # Threshold the Zernike 0 0 value?????
-
   # theta is the angle of the line connecting the 2 objects
   theta <- atan2((o2$Y - o1$Y), (o2$X - o1$X))
   
-
-  # project each vector onto this line
-  s1 <- o1$ZernikeMagnitude_1_1 * cos(o1$ZernikePhase_1_1 - theta)
-  s2 <- o2$ZernikeMagnitude_1_1 * cos(o2$ZernikePhase_1_1 - theta)
-  
-  # We know if s1 +ve and s2 -ve the vectors point towards each other, vive versa is away from each other
-  # Not interested in both +ve or both -ve
-  score <- s1 * s2
-  if(score >= 0){ # both +ve or both -ve
-    score <- 0
+  # Threshold the Zernike 0 0 value?????
+  threshold = 1E-5
+  if(o1$ZernikeMagnitude_0_0 < threshold || o2$ZernikeMagnitude_0_0 < threshold){
+    score = NA
+    s1 = 0
+    s2 = 0
   }
   else{
-    if(s1 > 0){  # implies s2 < 0
-      score <- abs(score) #   ->    <-
+  
+    # project each vector onto this line
+    s1 <- o1$ZernikeMagnitude_1_1 * cos(o1$ZernikePhase_1_1 - theta)
+    s2 <- o2$ZernikeMagnitude_1_1 * cos(o2$ZernikePhase_1_1 - theta)
+    
+    # We know if s1 +ve and s2 -ve the vectors point towards each other, vive versa is away from each other
+    # Not interested in both +ve or both -ve
+    score <- s1 * s2
+    if(score >= 0){ # both +ve or both -ve ->   -> or  <-   <-
+      score <- 0
     }
     else{
-      score <- -1 * abs(score) #   <- ->
+      if(s1 > 0){  # implies s2 < 0
+        score <- abs(score) # arrows together  ->   <-
+      }
+      else{
+        score <- -1 * abs(score) # arrows away  <-   ->
+      }
     }
+    
   }
-
+  
   return(c(score, s1, s2, theta*360/(2*3.142), o1$ZernikePhase_1_1*360/(2*3.142), o2$ZernikePhase_1_1*360/(2*3.142)))
 }
 
@@ -87,85 +95,110 @@ NR$obj1_y <- NA
 NR$obj2_x <- NA
 NR$obj2_y <- NA
 
-# Calculate scores between cells for 2 given channels
-channelA = 12
-channelB = 12
-chA_name <- ls(pattern = "HandN*")[channelA]     # relies on corrent file names!
-chB_name <- ls(pattern = "HandN*")[channelB]     # relies on corrent file names!
-chA_data <- get(chA_name)
-chB_data <- get(chB_name)
+# new data for all image scores (not cell interaction scores)
+Channel_A <- vector()
+Channel_B <- vector()
+Num_Interactions <- vector()
 
-for (i in 1:length(NR$First.Object.Number)){
-  
-  obj1 <- NR$First.Object.Number[i]
-  obj2 <- NR$Second.Object.Number[i]
-  
-  channelA_obj1 <- chA_data[obj1,]   # relies on one object per row in order in the channel data
-  channelB_obj2 <- chB_data[obj2,]
-  
-  s <- calculate_score3(channelA_obj1, channelB_obj2) # choose which score to calculate
-  
-  NR$score[i] <- s[1]
-  NR$s1[i] <- s[2]
-  NR$s2[i] <- s[3]
-  NR$theta[i] <- s[4]
-  NR$phase1[i] <- s[5]
-  NR$phase2[i] <- s[6]
-  
-  NR$obj1_x[i] <- channelA_obj1$X
-  NR$obj1_y[i] <- channelA_obj1$Y
-  NR$obj2_x[i] <- channelB_obj2$X
-  NR$obj2_y[i] <- channelB_obj2$Y
+
+# Calculate scores between cells for 2 given channels
+
+for(channelA in 1:12){
+  for(channelB in channelA:12){
+
+    chA_name <- ls(pattern = "HandN*")[channelA]     # relies on corrent file names!
+    chB_name <- ls(pattern = "HandN*")[channelB]     # relies on corrent file names!
+    chA_data <- get(chA_name)
+    chB_data <- get(chB_name)
+    
+    for (i in 1:length(NR$First.Object.Number)){
+      
+      obj1 <- NR$First.Object.Number[i]
+      obj2 <- NR$Second.Object.Number[i]
+      
+      if(obj1 > obj2) next    # there are repeats in the data, i.e. 1->12 and 12->1, remove repeats
+      
+      channelA_obj1 <- chA_data[obj1,]   # relies on one object per row in order in the channel data
+      channelB_obj2 <- chB_data[obj2,]
+      
+      s <- calculate_score3(channelA_obj1, channelB_obj2) # choose which score to calculate
+      
+      NR$score[i] <- s[1]
+      NR$s1[i] <- s[2]
+      NR$s2[i] <- s[3]
+      NR$theta[i] <- s[4]
+      NR$phase1[i] <- s[5]
+      NR$phase2[i] <- s[6]
+      
+      NR$obj1_x[i] <- channelA_obj1$X
+      NR$obj1_y[i] <- channelA_obj1$Y
+      NR$obj2_x[i] <- channelB_obj2$X
+      NR$obj2_y[i] <- channelB_obj2$Y
+    }
+    
+    # Categorise score
+    NR$interaction <- ifelse(NR$score > 0, "Y", 
+                             ifelse(NR$score == 0, NA, "N"))
+    
+    # remove repeats
+    NR_reduced <- subset(NR, First.Object.Number < Second.Object.Number)
+    
+    # Make data frame of just the interactions
+    NR_interactions <- subset(NR_reduced, interaction == "Y")
+    
+    if(channelA == channelB){
+      # visualise
+      name <- paste0("Zernike_with_Mask_", chA_name)
+      pdf(name, width = 7, height = 7)
+      
+      str_name<-paste0(channel_sub_folder, chA_name, "/Overlay.tiff") 
+      image <- readTIFF(str_name) 
+      grob <- rasterGrob(image, interpolate=TRUE)
+      
+      #plot Zernike with mask
+      p <- ggplot(chA_data, aes(X, Y)) +
+        annotation_custom(grob = grob, xmin=0, xmax=1000, ymin=0, ymax=-1000) +
+        scale_y_reverse() +
+        theme_classic() +
+        geom_point(color = "yellow", size = 0.25, stroke = 0) +
+        ggtitle(name) +
+        geom_segment(aes(x = X, y = Y, xend = Zernike_X, yend = Zernike_Y),
+                     arrow = arrow(length = unit(0.002, "npc")), size = 0.12,
+                     color = "white") +
+        geom_segment(data = NR_interactions, aes(x = obj1_x, y = obj1_y, xend = obj2_x, yend = obj2_y), color = "red",
+                    size = 0.15) #+
+                #    scale_color_gradient2(low = ("red"), mid = "white",
+                #                          high = ("purple"))
+      print(p)
+      
+      dev.off()
+    }
+    
+    # visualise just the interactions
+    name <- paste0("Zernike_interactions_", chA_name,  "_vs_", chB_name)
+    pdf(name, width = 7, height = 7)
+    
+    p <- ggplot(chA_data, aes(X, Y)) +
+      scale_y_reverse() +
+      theme_classic() +
+      geom_point(color = "yellow", size = 0.25, stroke = 0) +
+      ggtitle(name) +
+      geom_segment(data = NR_interactions, aes(x = obj1_x, y = obj1_y, xend = obj2_x, yend = obj2_y), color = "red",
+                   size = 0.15)
+    
+    print(p)
+    
+    dev.off()
+    
+    # store the number of interactions
+    Channel_A <- c(Channel_A, channelA)
+    Channel_B <- c(Channel_B, channelB)
+    Num_Interactions <- c(Num_Interactions, dim(NR_interactions)[1])
+
+  }
 }
 
-# Categorise score
-NR$interaction <- ifelse(NR$score > 0, "Y", 
-                         ifelse(NR$score == 0, NA, "N"))
-
-# visualise
-pdf("plot_scores.pdf", width = 7.9, height = 7)
-
-str_name<-paste0(channel_sub_folder, chA_name, "/Overlay.tiff") 
-image <- readTIFF(str_name) 
-grob <- rasterGrob(image, interpolate=TRUE)
-
-#plot Zernike with mask
-p <- ggplot(chA_data, aes(X, Y)) +
-  annotation_custom(grob = grob, xmin=0, xmax=1000, ymin=0, ymax=-1000) +
-  scale_y_reverse() +
-  theme_classic() +
-  geom_point(color = "yellow", size = 0.25, stroke = 0) +
-  ggtitle(paste("Zernike with Mask:", chA_name)) +
-  geom_segment(aes(x = X, y = Y, xend = Zernike_X, yend = Zernike_Y),
-               arrow = arrow(length = unit(0.002, "npc")), size = 0.12,
-               color = "white") +
-  geom_segment(data = NR, aes(x = obj1_x, y = obj1_y, xend = obj2_x, yend = obj2_y, color = interaction),
-              size = 0.15) #+
-          #    scale_color_gradient2(low = ("red"), mid = "white",
-          #                          high = ("purple"))
-print(p)
-
-dev.off()
-
-
-
-# geom_segment(data = NR, aes(x = obj1_x, y = obj1_y, xend = obj2_x, yend = obj2_y, color = score),
-#              size = 0.12) +
-#   scale_color_gradient2(low = ("red"), mid = "white",
-#                         high = ("blue"))
-# 
-
-# geom_segment(data = NR, aes(x = obj1_x, y = obj1_y, xend = obj2_x, yend = obj2_y, color = score),
-#              size = 0.12) +
-#   scale_color_gradient(high="red",low="blue")
-
-
-
-
-#  (data = NR, aes(x = obj1_x, y = obj1_y, xend = obj2_x, yend = obj2_y, col = score),
-#   size = 0.12) +
-# col = score
-
-
+number_of_interactions <- data.frame(Channel_A, Channel_B, Num_Interactions)
+write.csv(number_of_interactions, file = "number_of_interactions.csv")
 
 
